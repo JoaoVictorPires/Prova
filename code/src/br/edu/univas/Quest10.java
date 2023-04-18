29ede9ae67a9097b1f7f38cefa83bec255c01dae
joaovictorpires <joaovictorsantos0213@gmail.com>
2023-04-18 19:57:50 -0300

    Questão 10

---
+++ code/src/br/edu/univas/Quest10.java
@@ -0,0 +1,12 @@
+package br.edu.univas;
+
+public class Quest10 {
+    public static void main(String[] args) {
+        String comeca = "Big Smoke";
+        if (comeca.equals(comeca)){
+            System.out.println("Palavra existe");
+        }else{
+            System.out.println("Não existe");
+        }
+    }
+}
